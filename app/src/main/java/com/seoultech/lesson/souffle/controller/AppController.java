983d81d65cc76a77ab4b1316771e97107c5adce0
ruru14 <ljw2889@gmail.com>
2019-05-28 17:35:33 +0900

    [Update] AppController to Singleton pattern

--- app/src/main/java/com/seoultech/lesson/souffle/controller/AppController.java
+++ app/src/main/java/com/seoultech/lesson/souffle/controller/AppController.java
@@ -1,4 +1,14 @@
 package com.seoultech.lesson.souffle.controller;
 
 public class AppController {
+
+    private AppController(){ }
+
+    private static class LazyHolder{
+        public static final AppController instance = new AppController();
+    }
+
+    public static AppController getInstance(){
+        return LazyHolder.instance;
+    }
 }
